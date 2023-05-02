3bf92fd46ad0a00d308aea8cd9c52566ac3a8d35
Léo Niederberger <lniederberger@capcod.eu>
2023-05-02 13:44:40 +0200

    can navigate to soldier detail on ctrl+click

--- src/app/create-selected-soldiers/create-selected-soldiers.component.ts
+++ src/app/create-selected-soldiers/create-selected-soldiers.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, HostListener, Input, ViewChild } from '@angular/core';
 import { Soldier, equals } from '../interfaces/soldier';
 import { GameService } from '../services/game.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -6,6 +6,7 @@ import { SoldiersService } from '../services/soldiers.service';
 import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 import { NbValidator, NumberValidator } from '../numberValidator';
 import { combineLatest, forkJoin } from 'rxjs';
+import { getOrientationTuple, getShadowRootOrElse } from '@cds/core/internal';
 
 @Component({
   selector: 'app-create-selected-soldiers',
@@ -15,6 +16,18 @@ import { combineLatest, forkJoin } from 'rxjs';
 export class CreateSelectedSoldiersComponent {
 
   @Input() inputFilter: string = "";
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    this.ctrlPressed = event.ctrlKey;
+  }
+
+  @HostListener('document:keyup', ['$event'])
+  onKeyUp(event: KeyboardEvent) {
+    this.ctrlPressed = false;
+  }
+
+  ctrlPressed: boolean = false;
 
   radioFilter: string = "";
 
@@ -54,20 +67,10 @@ export class CreateSelectedSoldiersComponent {
     }).subscribe(results => {
       this.gameRebels = [...results.rebels];
       this.gameEmpires = [...results.empires];
-      if(this.firstSub)
+      if (this.firstSub)
         this.getSoldiers(results.empires, results.rebels);
       this.firstSub = false;
     });
-    // this.gameService.rebelsSubject
-    //   .subscribe(rebels => {
-    //     console.log(this.soldiers);
-    //     this.gameRebels = [...rebels];
-    // });
-    // this.gameService.empiresSubject
-    //   .subscribe(empires => {
-    //     console.log(this.soldiers);
-    //     this.gameEmpires = [...empires];
-    // });
   }
 
   createGame(a: any): void {
@@ -85,7 +88,6 @@ export class CreateSelectedSoldiersComponent {
 
   radioFilterSoldiers(filter: string, bool: any): void {
     if (bool.target.value) {
-
       this.radioFilter = filter;
       this.setFilter();
 
@@ -153,18 +155,17 @@ export class CreateSelectedSoldiersComponent {
       .subscribe(sld => {
         var list = sld.filter(sld => {
           var bool = true;
-          emps.forEach(emp =>
-            {
-              if(bool)
-                bool = !equals(emp, sld)
-            });
-          if(bool)
+          emps.forEach(emp => {
+            if (bool)
+              bool = !equals(emp, sld)
+          });
+          if (bool)
             rebs.forEach(reb => {
-              if(bool)
+              if (bool)
                 bool = !equals(reb, sld);
             })
           return bool;
-          });
+        });
         console.log(list);
         this.soldiers = list;
         this.filteredSoldiers = list;
@@ -182,13 +183,35 @@ export class CreateSelectedSoldiersComponent {
     this.overScrollView = null;
   }
 
-  addToGame(soldier: Soldier) {
-    this.removeFromSoldiers(soldier);
-    if (soldier.soldierType === "Rebel") {
-      this.gameService.addToRebels(soldier);
+  addToGame(soldier: Soldier): void {
+    if (this.ctrlPressed) {
+      this.goToSoldierInfo(soldier.id);
     }
     else {
-      this.gameService.addToEmpires(soldier);
+      this.removeFromSoldiers(soldier);
+      if (soldier.soldierType === "Rebel") {
+        this.gameService.addToRebels(soldier);
+      }
+      else {
+        this.gameService.addToEmpires(soldier);
+      }
+    }
+  }
+
+  removeFromGame(sld: Soldier): void {
+    if (this.ctrlPressed) {
+      this.goToSoldierInfo(sld.id);
+    }
+    else {
+      if (sld.soldierType == "Rebel") {
+        this.gameService.removeFromRebels(sld);
+      }
+      else {
+        this.gameService.removeFromEmpires(sld);
+      }
+
+      this.soldiers = [sld, ...this.soldiers];
+      this.setFilter();
     }
   }
 
@@ -200,20 +223,8 @@ export class CreateSelectedSoldiersComponent {
     this.setFilter();
   }
 
-  removeRebelFromGame(reb: Soldier): void {
-
-    this.gameService.removeFromRebels(reb);
-
-    this.soldiers = [reb, ...this.soldiers];
-    this.setFilter();
-  }
-
-  removeEmpireFromGame(emp: Soldier): void {
-
-    this.gameService.removeFromEmpires(emp);
-
-    this.soldiers = [emp, ...this.soldiers];
-    this.setFilter();
+  goToSoldierInfo(id: number): void {
+    this.router.navigateByUrl(`soldier/${id}`);
   }
 
   ngOnInit(): void {
